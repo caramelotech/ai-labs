e36732eee8177020873aca091b83d6a11d7925a9
Daniele Araújo <s.araujodaniele@gmail.com>
2026-04-13 21:50:33 -0300

    refactor: Simplify docs schema by removing optional fields

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,10 @@
 import { defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
-import { z } from 'astro:content';
 
 export const collections = {
   docs: defineCollection({
     loader: docsLoader(),
-    schema: docsSchema({
-      extend: z.object({
-        tags: z.array(z.string()).optional(),
-        date: z.coerce.date().optional(),
-      }),
-    }),
+    schema: docsSchema(),
   }),
 };
